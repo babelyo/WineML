f6674225d0b184556c7fc57806fbb1fbea8019e4
babelyo <babelyo136@gmail.com>
2026-02-08 20:28:24 +0100

    Second approach

--- DecisionTree.py
+++ DecisionTree.py
@@ -3,26 +3,40 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
+import joblib
 
 from sklearn.model_selection import GridSearchCV
+
+
+def quality_group(q):
+    if q <= 5:
+        return "low"
+    elif q <= 7:
+        return "medium"
+    else:
+        return "high"
+
 
 df = pd.read_csv('winequality_white.csv', sep=';')
 print(df)
 print(df.quality.value_counts())
 
 X = df.iloc[:,:-1]  # wszystkie kolumny, bez ostatniej
-y = df.quality  # kolumna target
+y = df.quality  # kolumna quality
+y_grouped = y.apply(quality_group)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = DecisionTreeClassifier(criterion= 'gini', max_depth= 8, max_features= 8, min_samples_split= 8, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y_grouped, test_size=0.2, random_state=42)
+model = DecisionTreeClassifier(criterion= 'gini', max_depth= 8, max_features= 10, min_samples_split= 5, random_state=42)
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
 print(pd.DataFrame(model.feature_importances_, X.columns))  #które cechy najwazniejsze
 print(classification_report(y_test, model.predict(X_test)))
-print(f'liczba cech {X.shape[1]}')
+joblib.dump(model, 'moj_tree_classifier.model')
+
 # Szukanie nalepszych parametrów dla drzewa decyzyjnego, zględem najlepszego dopasowania.
 '''
+print(f'liczba cech {X.shape[1]}')
 model = DecisionTreeClassifier()
 params = {
     'max_depth': range(2, 10),
---
+++ Random Forest.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+def quality_group(q):
+    if q <= 5:
+        return "low"
+    elif q <= 7:
+        return "medium"
+    else:
+        return "high"
+
+
+df = pd.read_csv("winequality_white.csv", sep=';')
+
+X = df.iloc[:, :-1]    # wszystkie cechy
+y = df.quality
+y_grouped = y.apply(quality_group)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y_grouped, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(n_estimators=100, max_depth=8, max_features=8, min_samples_split=8, random_state=42, class_weight="balanced")
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+
+# zapis i załadowanie modelu
+import joblib
+# joblib.dump(model, 'moj_tree_classifier.model')  #zapis modelu do pliku
+model_new = joblib.load('moj_tree_classifier.model')  #wczytanie modelu
+print(X_test)
+print(X_test.iloc[4:10,:])   # wybieram jeden wiersz
+print(model_new.predict(X_test.iloc[4:10,:]))  # liczę
